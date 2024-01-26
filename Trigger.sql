ddd4e6ece57420c7f23431d463522a594dcb2b08
AkberliElnur <126491959+Elnur107@users.noreply.github.com>
2024-01-26 10:09:00 +0400

    Add files via upload

---
+++ Trigger.sql
@@ -0,0 +1,39 @@
+﻿CREATE TRIGGER trgShippersOperations
+ON Shippers
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @OperationType NVARCHAR(50);
+
+    IF EXISTS (SELECT 1 FROM inserted)
+    BEGIN
+        IF EXISTS (SELECT 1 FROM deleted)
+        BEGIN
+            SET @OperationType = 'Güncelleme İşlemi Yapıldı';
+        END
+        ELSE
+        BEGIN
+            SET @OperationType = 'Kayıt Ekleme İşlemi Yapıldı';
+        END
+    END
+    ELSE
+    BEGIN
+        SET @OperationType = 'Silme İşlemi Yapıldı';
+    END
+
+    PRINT @OperationType;
+END;
+
+
+INSERT INTO Shippers (ShipperID, CompanyName, Phone)
+VALUES (4, 'New Shipper', '123-456-7890');
+
+
+UPDATE Shippers SET CompanyName = 'Updated Shipper' WHERE ShipperID = 4;
+
+
+DELETE FROM Shippers WHERE ShipperID = 4;
+
+
